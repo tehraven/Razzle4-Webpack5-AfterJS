74e9ed0b44022f1a958d04c103e37f5488b14b5b
Anthony M Powers <tehraven@gmail.com>
2021-07-26 20:25:05 -0700

    Typescript working

--- src/About.js
+++ src/About.js
@@ -1,8 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+
+import SomeTypescriptComponent from "./SomeTypescriptComponent";
 
 class About extends Component {
   render() {
-    return <div>about</div>;
+    return (
+      <>
+        <div>about</div>
+        <SomeTypescriptComponent />
+      </>
+    );
   }
 }
 
---
+++ src/SomeTypescriptComponent.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default () => {
+  const testing: string = "I am a typed variable";
+
+  return <p>{testing}</p>;
+};
